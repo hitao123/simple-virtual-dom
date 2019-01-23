50e24e4cfa22ed4da45ff83c8f15ad98634d701f
hitao123 <hitao1234@gmail.com>
2019-01-23 23:43:24 +0800

    add diff

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
-exports.el = require('./lib/element');
-exports.diff = require('./lib/diff');
-exports.patch = require('./lib/patch');
+exports.el = require('./lib/element')
+exports.diff = require('./lib/diff')
+exports.patch = require('./lib/patch')
